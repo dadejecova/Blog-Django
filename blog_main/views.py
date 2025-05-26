78583dd50f9c3fdf83f59f577b3a6d44944d3ac7
Daniel <danielcoellovargas@gmail.com>
2025-05-25 23:00:44 -0500

    Display Featured Posts

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse ### delete this line
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Blog, Category
 
 
 def home(request):
     categories = Category.objects.all()
+    features_posts = Blog.objects.filter(is_featured=True).order_by('updated_at')
     context = {
         'categories': categories,
+        'featured_posts': features_posts,
     }
     print(categories)  # Debugging line to check categories
     return render(request, 'home.html', context)
