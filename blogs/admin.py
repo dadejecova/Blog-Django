e02350d592c206278727f7a2616dd922b521f200
Daniel <danielcoellovargas@gmail.com>
2025-05-25 21:13:27 -0500

    prepopulte Slugs

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Category, Blog
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)} # Automatically generate slug from title
+ 
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
