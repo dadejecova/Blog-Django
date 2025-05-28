7bd1b35e366b0167a6f8049a3fc5a970f2fd9f08
Daniel <danielcoellovargas@gmail.com>
2025-05-27 23:24:04 -0500

    Custom Error Page
    Debug

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from blogs.models import Blog, Category
 
@@ -7,11 +7,13 @@ def posts_by_category(request, category_id):
     # Fetch posts by category_id
     posts = Blog.objects.filter(status='Published', category = category_id)
     # use try-except to handle the case where category_id does not exist
-    try:
-        category = Category.objects.get(pk=category_id)
-    except:
+#    try:
+#        category = Category.objects.get(pk=category_id)
+#    except:
         # redirect user to home page if category not found
-        return redirect('home')
+#        return redirect('home')
+    category = get_object_or_404(Category, pk=category_id)
+    # Render the posts in the template
     context = {
         'posts': posts,
         'category': category,
