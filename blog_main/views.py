b75a1d9af776a69cd83d7dcfcde85224ad7e3a3f
Daniel <danielcoellovargas@gmail.com>
2025-05-28 19:05:26 -0500

    Inheritance base html
    base.html

--- blog_main/views.py
+++ blog_main/views.py
@@ -6,6 +6,7 @@ from blogs.models import Blog, Category
 def home(request):
     categories = Category.objects.all()
     features_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
+    
     posts = Blog.objects.filter(is_featured=False, status='Published')
     
     
