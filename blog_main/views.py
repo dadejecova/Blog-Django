552637e3b72a88f2638b5644cd4bf56c1b8a2db6
Daniel <danielcoellovargas@gmail.com>
2025-05-28 19:17:07 -0500

    Context processor

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,16 +4,15 @@ from blogs.models import Blog, Category
 
 
 def home(request):
-    categories = Category.objects.all()
-    features_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
     
+    features_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
     
     
     context = {
-        'categories': categories,
+
         'featured_posts': features_posts,
         'posts': posts,
     }
-    print(categories)  # Debugging line to check categories
+
     return render(request, 'home.html', context)
