1556f081760573aaaf8bc6ea21d6485698cdd602
Daniel <danielcoellovargas@gmail.com>
2025-06-01 20:21:15 -0500

    About us Model
    Feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse ### delete this line
 from django.shortcuts import render
+from assignment.models import About
 from blogs.models import Blog, Category
 
 
@@ -8,11 +9,17 @@ def home(request):
     features_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
     
-    
+    # Fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+    # Fetch all categories
     context = {
 
         'featured_posts': features_posts,
         'posts': posts,
+        'about': about,
     }
 
     return render(request, 'home.html', context)
