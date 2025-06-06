afbead1fa4c53ef2f2c7a2ad3072ab6446f66c9e
Daniel <danielcoellovargas@gmail.com>
2025-06-06 16:26:35 -0500

    Login & logout

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     # search endpoint
     path('blogs/search/', Blog_views.search, name='search'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- blog_main/views.py
+++ blog_main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import redirect, render
 from assignment.models import About
 from blog_main.forms import RegistrationForm
 from blogs.models import Blog, Category
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     
@@ -39,3 +40,26 @@ def register(request):
         'form': form,
     }
     return render(request, 'register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            # Login user
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+    form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
