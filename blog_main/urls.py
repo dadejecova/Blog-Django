d554b1bf5ae86ef1ee1d51ddeba6c7f1b1cf3ad3
Daniel <danielcoellovargas@gmail.com>
2025-06-06 11:37:28 -0500

    Search Filter

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for blog_main project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 from . import views
@@ -26,4 +11,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
     path('<slug:slug>/', Blog_views.blogs, name='blogs'),
+    # search endpoint
+    path('blogs/search/', Blog_views.search, name='search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from blogs.models import Blog, Category
 
+from django.db.models import Q
 # Create your views here.
 def posts_by_category(request, category_id):
     # Fetch posts by category_id
@@ -17,6 +18,7 @@ def posts_by_category(request, category_id):
     context = {
         'posts': posts,
         'category': category,
+
     }
     return render(request,'posts_by_category.html', context)
 
@@ -27,3 +29,20 @@ def blogs(request, slug):
         'single_blog': single_blog,
     }
     return render(request, 'blogs.html', context)
+
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    
+    blogs = Blog.objects.filter(
+        Q(title__icontains=keyword) | 
+        Q(short_description__icontains=keyword) |
+        Q(blog_body__icontains=keyword),
+        status='Published'
+
+    )
+    print(blogs)
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'search.html', context)
