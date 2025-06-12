459bab4958095d544131ac9b3b9dbd9df5c2d0c8
Daniel <danielcoellovargas@gmail.com>
2025-06-11 22:11:27 -0500

    update - Unique Slug

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render
-
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
-
 from dashboards.forms import BlogPostForm, CategoryForm
+from django.template.defaultfilters import slugify
 
 @login_required(login_url='login')
 # Create your views here.
@@ -68,9 +67,10 @@ def add_post(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            # Save the post with the current user as the author
             post.save()
-            
+            title = form.cleaned_data['title']
+            post.slug = slugify(title) + '-' + str(post.id)
+            post.save()
             return redirect('posts')
         else:
             print("Form is not valid")
