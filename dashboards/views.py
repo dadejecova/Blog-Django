22761540eb4a5431e39bf1dc52f37d2e5ef380d5
Daniel <danielcoellovargas@gmail.com>
2025-07-08 20:28:29 -0500

    DeleteUserForm
    Url_ToDeleteUsers

--- dashboards/views.py
+++ dashboards/views.py
@@ -146,3 +146,8 @@ def edit_user(request, pk):
         'form': form,
     }
     return render(request, 'dashboard/edit_user.html', context)
+
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
